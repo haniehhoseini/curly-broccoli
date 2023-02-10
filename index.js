5a99d476b30924a9f8708b9fa0bbf7a0f55d5c08
mrympj <work.vahedi>
2023-02-10 22:40:32 +0330

    revert: revert unintentional commits

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
+const { prisma } = require("./prisma");
 
 // Load environment variables from .env file
 require("dotenv").config();
