c078d27ac5e446a511c141562838ea7dca0da6f2
haniehhoseini <work.vahedi>
2023-02-11 20:31:34 +0330

    fix: fix products search page number format

--- routes/api/product/getProducts.js
+++ routes/api/product/getProducts.js
@@ -1,13 +1,13 @@
 const { prisma } = require("../../../prisma");
 
 module.exports = async (req, res) => {
-  const { q, page } = req.query || {};
+  const { q, page = 1  } = req.query || {};
 
   const products = await prisma.t_Product.findMany({
     where: {
       OR: [{ Description: { contains: q } }, { Title: { contains: q } }],
     },
-    skip: page * 10,
+    skip: +page * 10,
     take: 10,
     orderBy: { CreateDate: "desc" },
   });
