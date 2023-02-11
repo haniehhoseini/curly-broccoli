08f9a1ffb5ce0e43cdb751b492804e63d19864d6
haniehhoseini <work.vahedi>
2023-02-11 13:27:56 +0330

    feat: add first api check health

--- index.js
+++ index.js
@@ -10,9 +10,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.send("Hello Worlds!");
-});
+app.use("/api", require("./routes").api);
 
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${process.env.PORT}!`);
